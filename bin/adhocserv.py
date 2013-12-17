94a1dad2d2148e9c3a44b61463321fb7a61207b5
Christer Bernérus <bernerus@CHALMERS.SE>
2013-12-17 10:34:13 +0000

    Find the modules automatically in the server directory, import them and register the managers and functions at runtime.
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@110 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

--- bin/adhocserv.py
+++ bin/adhocserv.py
@@ -6,36 +6,16 @@ import sys
 env_prefix = "ADHOC_"
 
 # Automagic way to find out the home of adhoc.
-adhoc_home = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+"/.."
+adhoc_home = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/.."
 #print "ADHOC_HOME=", adhoc_home
 os.environ[env_prefix + "RUNTIME_HOME"] = adhoc_home  # Export as env variable ADHOC_RUNTIME_HOME if needed outside server
 
 sys.path.append(adhoc_home)
 sys.path.append(os.path.join(adhoc_home, 'server'))
 
-#for p in sys.path:
-    #print p
-#import rpcc.server
-#import rpcc.session
-#import rpcc.database
-#import rpcc.authentication
-
-#from model import *
-#from exttype import *
-#from dhcp_manager import DHCPManager, DhcpdConf, DhcpXfer
-
-
 from rpcc.server import Server
 from rpcc.database import MySQLDatabase
-import dhcp 
-import util
-import shared_network
-import dhcp_server
-import optionspace
-import room
-import building
-import global_options
-
+import re
 import rpcc
 
 
@@ -44,27 +24,33 @@ class AdHocServer(Server):
     service_name = "AdHoc"
     major_version = 0
     minor_version = 1
+    
 srv = AdHocServer("nile.medic.chalmers.se", 12121)
 
 srv.enable_database(MySQLDatabase)
 srv.database.check_rpcc_tables()
+
+scriptdir = os.path.dirname(os.path.realpath(__file__))
+(scriptparent, tail) = os.path.split(scriptdir)
+serverdir = os.path.join(scriptparent, "server")
+
 srv.register_manager(rpcc.session.DatabaseBackedSessionManager)
 srv.register_manager(rpcc.authentication.NullAuthenticationManager)
-srv.register_manager(dhcp.DHCPManager)
-srv.register_manager(shared_network.NetworkManager)
-srv.register_manager(dhcp_server.DHCPServerManager)
-srv.register_manager(optionspace.OptionspaceManager)
-srv.register_manager(room.RoomManager)
-srv.register_manager(building.BuildingManager)
-srv.register_manager(global_options.GlobalOptionManager)
-srv.register_functions_from_module(dhcp)
-srv.register_functions_from_module(util)
-srv.register_functions_from_module(shared_network)
-srv.register_functions_from_module(dhcp_server)
-srv.register_functions_from_module(optionspace)
-srv.register_functions_from_module(room)
-srv.register_functions_from_module(building)
-srv.register_functions_from_module(global_options)
+
+# Find the server directory and register all managers and functions in the modules found.
+
+for file in os.listdir(serverdir):
+    mo = re.match(r"^([a-z_]+).py$", file)
+    if not mo:
+        continue
+    module = __import__(mo.group(1))
+    for name, obj in inspect.getmembers(module):
+        if inspect.isclass(obj):
+            if issubclass(obj, rpcc.model.Manager):
+                srv.register_manager(obj)
+                break
+    srv.register_functions_from_module(module)
+             
 
 srv.enable_documentation()
 srv.enable_static_documents(os.path.join(adhoc_home, 'docroot'))
