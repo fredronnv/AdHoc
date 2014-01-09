f6cfbd4a5fe4092e4c99b36f992cb0717e548b06
Christer Bernérus <bernerus@CHALMERS.SE>
2014-01-09 07:12:30 +0000

    Moved the experimental ExtDict type out of RPCC and put it into AdHoc's util package for now.
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@134 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

--- server/rpcc/exttype.py
+++ server/rpcc/exttype.py
@@ -834,45 +834,6 @@ class ExtStruct(ExtType):
             out(optelem, sorted(opt), self.optional)
 
 
-class ExtDict(ExtStruct):
-    typ = None
-
-    def __init__(self, typ=None):            
-        if typ is not None:
-            if self.typ is not None:
-                raise TypeError("When an ExtDict subclass has its .typ set, you cannot override it on instantiation. You use an ExtDict subclass just like an ExtString or ExtInteger subclass.")            
-            self.typ = typ
-
-        if self.typ is not None:
-            self.name = self.typ.name + '-dict'
-        else:
-            self.name = None
-    
-    def check(self, function, rawval):
-        if not isinstance(rawval, dict):
-            raise ExtExpectedStructError(value=rawval)
-
-        for (key, val) in rawval.items():
-            try:
-                ExtType.instance(self.typ).check(function, val)
-            except ExtError as e:
-                e.add_traceback(key)
-                raise
-            
-    def output(self, function, value):
-        converted = {}
-        inval = value.copy()
-
-        for (key, subval) in inval.items():
-           
-            typ = ExtType.instance(self.typ)
-            try:
-                converted[key] = typ.output(function, subval)
-            except ExtOutputError as e:
-                e.add_trace(self, "While converting key '%s'" % (key,))
-                raise
-
-        return converted 
 ###
 # OrNull
 #
@@ -902,8 +863,6 @@ class ExtOrNull(ExtType):
         else:
             self.name = ExtType.instance(self.typ)._name() + '|null'
 
-            
-        
     def _subtypes(self):
         return [(None, self.typ)]
 
--- server/util.py
+++ server/util.py
@@ -1,5 +1,43 @@
 #!/usr/bin/env python2.6
 from rpcc.exttype import *
-from rpcc.function import Function
 
 
+class ExtDict(ExtStruct):
+    typ = None
+
+    def __init__(self, typ=None):            
+        if typ is not None:
+            if self.typ is not None:
+                raise TypeError("When an ExtDict subclass has its .typ set, you cannot override it on instantiation. You use an ExtDict subclass just like an ExtString or ExtInteger subclass.")            
+            self.typ = typ
+
+        if self.typ is not None:
+            self.name = self.typ.name + '-dict'
+        else:
+            self.name = None
+    
+    def check(self, function, rawval):
+        if not isinstance(rawval, dict):
+            raise ExtExpectedStructError(value=rawval)
+
+        for (key, val) in rawval.items():
+            try:
+                ExtType.instance(self.typ).check(function, val)
+            except ExtError as e:
+                e.add_traceback(key)
+                raise
+            
+    def output(self, function, value):
+        converted = {}
+        inval = value.copy()
+
+        for (key, subval) in inval.items():
+           
+            typ = ExtType.instance(self.typ)
+            try:
+                converted[key] = typ.output(function, subval)
+            except ExtOutputError as e:
+                e.add_trace(self, "While converting key '%s'" % (key,))
+                raise
+
+        return converted 
