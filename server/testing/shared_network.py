b5d424096e6af1c15af17a542030f93d933d9d6c
Christer Bernérus <bernerus@CHALMERS.SE>
2014-01-16 14:33:05 +0000

    Added option set/unset tests. Diffetentiate tests between Unauth and Auth tests.
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@149 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

--- server/testing/shared_network.py
+++ server/testing/shared_network.py
@@ -14,9 +14,9 @@ class T0400_networkList(UnAuthTests):
             ret = self.proxy.network_dig({}, {"network": True, "info": True, "authoritative": True, "mtime": True, "changed_by":True})
             
             assert len(ret) > 90, "Too few networks returned"
-            for n in ret:
-                print n
-                print n.authoritative, n.network, n.info, n.mtime, n.changed_by
+            #for n in ret:
+                #print n
+                #print n.authoritative, n.network, n.info, n.mtime, n.changed_by
   
   
 class T0410_networkFetch(UnAuthTests):
@@ -32,72 +32,132 @@ class T0410_networkFetch(UnAuthTests):
             assert "authoritative" in ret, "Key authoritative missing in returned struct from network_fetch"
             
             
-class T0420_networkCreate(UnAuthTests):
+class T0420_networkCreate(AuthTests):
     """ Test network_create """
     
-    def do(self):  
-        if self.proxy != self.superuser:
-            return
+    def do(self):
         try:
             self.superuser.network_destroy('network_test')
         except:
             pass
-        with AssertAccessError(self):
-            self.proxy.network_create('network_test', False, "Testnätverk")
-            ret = self.superuser.network_fetch('network_test', {"network": True, "info": True, "authoritative": True})
-            assert "network" in ret, "Key network missing in returned struct from network_fetch"
-            assert "info" in ret, "Key authoritative missing in returned struct from network_fetch"
-            assert "authoritative" in ret, "Key authoritative missing in returned struct from network_fetch" 
-            assert ret.network == "network_test", "Bad network, is % should be %s" % (ret.network, "network_test")
-            assert ret.authoritative == False, "Authoritative is " + ret.authoritative + " but should be False"
+        try:
+            with AssertAccessError(self):
+                self.proxy.network_create('network_test', False, "Testnätverk")
+                ret = self.superuser.network_fetch('network_test', {"network": True, "info": True, "authoritative": True})
+                assert "network" in ret, "Key network missing in returned struct from network_fetch"
+                assert "info" in ret, "Key authoritative missing in returned struct from network_fetch"
+                assert "authoritative" in ret, "Key authoritative missing in returned struct from network_fetch" 
+                assert ret.network == "network_test", "Bad network, is % should be %s" % (ret.network, "network_test")
+                assert ret.authoritative == False, "Authoritative is " + ret.authoritative + " but should be False"
+        finally:
+            try:
+                self.superuser.network_destroy('network_test')
+            except:
+                pass
         
         
-class T0430_networkDestroy(UnAuthTests):
+class T0430_networkDestroy(AuthTests):
     """ Test network destroy """
     
     def do(self):
-        if self.proxy != self.superuser:
-            return
-        with AssertAccessError(self):
-            self.proxy.network_destroy('network_test')
-            with AssertRPCCError("LookupError::NoSuchNetwork", True):
-                self.superuser.network_fetch('network_test', {"network": True})
+        self.superuser.network_create('network_test', False, "Testnätverk")
+        try:
+            with AssertAccessError(self):
+                self.proxy.network_destroy('network_test')
+                with AssertRPCCError("LookupError::NoSuchNetwork", True):
+                    self.superuser.network_fetch('network_test', {"network": True})
+        finally:
+            try:
+                self.superuser.network_destroy('network_test')
+            except:
+                pass
+            
         
-        
-class T0440_networkSetAuthoritative(UnAuthTests):
+class T0440_networkSetAuthoritative(AuthTests):
     """ Test setting authoritative flag on a network"""
     
     def do(self):
-        if self.proxy != self.superuser:
-            return
         self.superuser.network_create('network_test', False, "Testnätverk 2")
-        with AssertAccessError(self):
-            try:
+        try:
+            with AssertAccessError(self):
                 self.proxy.network_update('network_test', {"authoritative": True})
                 nd = self.superuser.network_fetch('network_test', {"network": True, "info": True, "authoritative": True})
                 assert nd.network == "network_test", "Bad network"
                 assert nd.authoritative, "Bad autoritativity"
                 assert nd.info == "Testnätverk 2", "Bad info"
-            finally:
+        finally:
+            try:
                 self.superuser.network_destroy('network_test')
+            except:
+                pass
                 
                 
-class T0450_networkSetInfo(UnAuthTests):
+class T0450_networkSetInfo(AuthTests):
     """ Test setting info on a network"""
     
     def do(self):
-        if self.proxy != self.superuser:
-            return
         self.superuser.network_create('network_test', False, "Testnätverk 2")
-        with AssertAccessError(self):
-            try:
+        try:
+            with AssertAccessError(self):
                 self.proxy.network_update('network_test', {"info": "Provnät 1"})
                 nd = self.superuser.network_fetch('network_test', {"network": True, "info": True, "authoritative": True})
                 assert nd.network == "network_test", "Bad network"
                 assert not nd.authoritative, "Bad autoritativity"
                 assert nd.info == "Provnät 1", "Bad info"
-            finally:
+        finally:
+            try:
                 self.superuser.network_destroy('network_test')
+            except:
+                pass
+                
+                
+class T0460_NetworkSetOption(AuthTests):
+    """ Test setting options on a network"""
+    
+    def do(self):
+        try:
+            self.superuser.network_destroy('network_test')
+        except:
+            pass
+
+        self.superuser.network_create('network_test', False, "Testnätverk 2")
         
+        try:
+            with AssertAccessError(self):
+                self.proxy.network_option_set("network_test", "subnet-mask", "255.255.255.0")
+                nd = self.superuser.network_fetch('network_test', {"info": True, "network": True, "options": True})
+                assert nd.network == "network_test", "Bad network id"
+                assert nd.info == "Testnätverk 2", "Bad info"
+                assert nd.options["subnet-mask"] == "255.255.255.0", "Bad subnet-mask in options"
+                
+        finally:
+            try:
+                self.superuser.network_destroy('network_test')
+            except:
+                pass
+                
+                
+class T0470_NetworkUnsetOption(AuthTests):
+    """ Test unsetting options on a network"""
+    
+    def do(self):
+        self.superuser.network_create('network_test', False, "Testnätverk 2")
+        
+        try:
+            with AssertAccessError(self):
+
+                self.proxy.network_option_set("network_test", "subnet-mask", "255.255.255.0")
+                self.proxy.network_option_unset("network_test", "subnet-mask")
+                nd = self.superuser.network_fetch("network_test", {"info": True, "network": True, "options": True})
+                assert nd.network == "network_test", "Bad network id"
+                assert nd.info == "Testnätverk 2", "Bad info"
+                assert "subnet-mask" not in nd.options, "Subnet-mask still in options"
+                
+        finally:
+            try:
+                self.superuser.network_destroy('network_test')
+            except:
+                pass
+
 if __name__ == "__main__":
     sys.exit(main())
