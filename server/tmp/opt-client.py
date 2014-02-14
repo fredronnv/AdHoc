a5d6fb3c48a72eeb3674e6fed5e71f527dbebe96
Viktor Fougstedt <viktor@CHALMERS.SE>
2014-02-14 14:47:22 +0000

    This is an initial checkin of an Optionset model with matching manager.
    Currently a standalone server, it is to be incorporated into the rest
    of AdHoc soon, therefore the "tmp" folder.
    
    The Optionset model has dynamic attributes, which the OptionsetManager
    reads from the database on server startup. All templates, updates and
    searches are added dynamically at the same time (using RPCC's new
    @auto_template/@auto_update/@auto_search feature).
    
    Still to do is to implement the methods to create, change and remove
    options in the latest API version if the server is at "development"
    deploy level (detecting this reliably is also to be included), and
    methods to list all options for a particular API version.
    
    Note: The client expects there to be two optionsets with ID:s 1 and
    2. It updates, fetches and digs them to test the correct workings of
    the server part.
    
    
    
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@184 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

---
+++ server/tmp/opt-client.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2.6
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append("/home/viktor/AdHoc/trunk/client")
+
+import rpcc_client
+from rpcc_client import RPCCValueError, RPCCLookupError
+
+rpcc = rpcc_client.RPCC("http://localhost:7310", 0)
+
+tmpl = {"b1": True, "b2": True, "i1": True, "i2": True, "s1": True, "s2": True}
+print rpcc.optionset_fetch(2, tmpl)
+
+print rpcc.optionset_fetch(1, tmpl)
+
+print rpcc.optionset_update(1, {"b1": False, "i1": 2, "s1": "3", "b2": True})
+print rpcc.optionset_fetch(1, tmpl)
+
+print rpcc.optionset_update(1, {"b1": True, "i1": 1, "s1": "foo", "b2": None})
+print rpcc.optionset_fetch(1, tmpl)
+
+print rpcc.server_documentation("optionset_dig")
+
+print "[1]", rpcc.optionset_dig({"s1_pattern": "fo*"}, {"optionset": True})
+print "[]", rpcc.optionset_dig({"s1_pattern": "bo*"}, {"optionset": True})
+rpcc.optionset_update(1, {"b2": None})
+print "[]", rpcc.optionset_dig({"b2_is_set": True}, {"optionset": True})
+rpcc.optionset_update(1, {"b2": True})
+rpcc.optionset_update(2, {"b2": None})
+print "[1]", rpcc.optionset_dig({"b2_is_set": True}, {"optionset": True})
+print "[2]", rpcc.optionset_dig({"b2_is_not_set": True}, {"optionset": True})
+
+rpcc.optionset_update(1, {"b2": True})
+rpcc.optionset_update(2, {"b2": False})
+print "[1]", rpcc.optionset_dig({"b2": True}, {"optionset": True})
+print "[2]", rpcc.optionset_dig({"b2": False}, {"optionset": True})
+
