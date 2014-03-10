20cc222cb4631cce237ece90068ec31592e55f4b
Christer Bernérus <bernerus@CHALMERS.SE>
2014-03-10 11:36:21 +0000

    Picked up viktor's rechent changes. Added username/password authentication via kerberos.
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@241 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

--- server/rpcc/authentication.py
+++ server/rpcc/authentication.py
@@ -11,19 +11,27 @@ normally call .auth_session() and .deauth_session() respectively.
 
 import exterror
 import model
-
+import os
 
 class AuthenticationManager(model.Manager):
-    """Supplies two default methods: 
+    """Supplies three default methods: 
        .login(session, username, password)
+       .login_krb5(session, token)
        .logout(session)
 
     Implementations MUST set the 'authuser' attribute of the sessions
     if authentication succeeds, but are allowed to set other attributes 
-    as well."""
+    as well.
+    
+    Subclasses may add additional login methods.
+    """
 
     name = "authentication_manager"
     models = None
+
+    # Set to the realm(s) from which we accept logins. This is the
+    # part of a Kerberos principal following the @-sign.
+    krb5_realms = []
 
     def model(self, oid):
         # DO NOT IMPLEMENT THIS - it is a model-less manager!
@@ -35,16 +43,65 @@ class AuthenticationManager(model.Manager):
     def logout(self, session):
         raise NotImplementedError()
 
+    @classmethod
+    def has_krb5(self):
+        try:
+            import kerberos
+            return True
+        except:
+            return False
+
 
 class NullAuthenticationManager(AuthenticationManager):
-    def login(self, session, username, password):
+    def login_null(self, session, username, password):
         if username == password:
             session.set("authuser", username)
         else:
             raise exterror.ExtAuthenticationFailedError()
+        
+    def login(self, session, username, password):
+        if AuthenticationManager.has_krb5():
+            import kerberos
+            krb_realm=os.environ.get('ADHOC_KRB_REALM','CHALMERS.SE')
+            service = "krbtgt/" + self.function.server.instance_address
+            if kerberos.checkPassword(username, password, service, krb_realm):
+                    session.set("authuser", username)
+            else:
+                raise exterror.ExtAuthenticationFailedError()
+        else:
+            raise exterror.ExtAuthenticationFailedError()
+
+    def login_krb5(self, session, token):
+        ctx = None
+        try:
+            server_principal = "HTTP@" + self.function.server.instance_address
+            (res, ctx) = kerberos.authGSSServerInit(server_principal)
+            res = kerberos.authGSSServerStep(ctx, token)
+            if res != kerberos.AUTH_GSS_COMPLETE:
+                raise default_error.ExtAccessDeniedError()
+            
+            principal = kerberos.authGSSServerUserName(ctx)
+            sys.stderr.write("--> Kerberos SPNEGO auth: %s\n" % (authprinc,))
+            if '@' in principal:
+                priminst, realm = authprinc.split('@')[0]
+                if "/" in priminst:
+                    primary, instance = priminst.split("/")
+                else:
+                    primary, instance = priminst, ""
+            else:
+                primary, instance, realm = principal, "", ""
+
+            session.set("authuser", primary)
+            session.set("authinst", instance)
+            session.set("authrealm", realm)
+        finally:
+            if ctx:
+                kerberos.authGSSServerClean(ctx)
 
     def logout(self, session):
         session.unset("authuser")
+        session.unset("authinst")
+        session.unset("authrealm")
 
 
 class SuperuserOnlyAuthenticationManager(AuthenticationManager):
