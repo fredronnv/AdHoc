3243aa42dbc89af7ef95a9d9b2a01da560958657
Christer Bernérus <bernerus@CHALMERS.SE>
2014-03-11 13:31:10 +0000

    More packaging, not done yet...
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@249 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

--- bin/adhocserv.py
+++ bin/adhocserv.py
@@ -27,44 +27,97 @@ class AdHocServer(Server):
     
     superuser_guard = AdHocSuperuserGuard
     
-       
-srv = AdHocServer("nile.its.chalmers.se", 12121)
+    
+    
+class StartMe(object):
+    def __init__(self, host, port, generic_password=None, enable_ssl=False):
 
-srv.enable_database(MySQLDatabase)
-srv.database.check_rpcc_tables()
+        if enable_ssl:
+            print "Enabling SSL"
+            keyfile = os.environ.get('RPCC_SERVER_SSL_KEYFILE', 'etc/xmlrpc_server.key')
+            certfile = os.environ.get('RPCC_SERVER_SSL_CERTFILE', 'etc/xmlrpc_server.cert')
+            chainfile = os.environ.get('RPCC_SERVER_SSL_CHAINFILE', None)
 
-scriptdir = os.path.dirname(os.path.realpath(__file__))
-(scriptparent, tail) = os.path.split(scriptdir)
-serverdir = os.path.join(scriptparent, "server")
+            #print "Keyfile =",keyfile
+            #print "Certfile =",certfile
+            #print "Chainfile =",chainfile
+        else: 
+            certfile = None
+            chainfile = None
+            keyfile = None
+            print "Disabling SSL"
+    
+        srv = AdHocServer(host,
+                                  port,
+                                  keyfile, 
+                                  certfile,
+                                  chainfile=chainfile,
+                                  session_class=pdbrpc.PDBRPCSession,
+                                  disable_ssl=not enable_ssl,
+                                  generic_password=generic_password)
 
-srv.register_manager(session.DatabaseBackedSessionManager)
+        srv.enable_database(MySQLDatabase)
+        srv.database.check_rpcc_tables()
 
-srv.register_manager(event.EventManager)
+        scriptdir = os.path.dirname(os.path.realpath(__file__))
+        (scriptparent, tail) = os.path.split(scriptdir)
+        serverdir = os.path.join(scriptparent, "server")
 
-# Find the server directory and register all managers and functions in the modules found.
-seen_managers = []  # Avoid duplicating registrations. This can happen if managers are imported from other objects.
+        srv.register_manager(session.DatabaseBackedSessionManager)
+        
+        srv.register_manager(event.EventManager)
+        
+        # Find the server directory and register all managers and functions in the modules found.
+        seen_managers = []  # Avoid duplicating registrations. This can happen if managers are imported from other objects.
+        
+        for file in os.listdir(serverdir):
+            mo = re.match(r"^([a-z_]+).py$", file)
+            if not mo:
+                continue
+            module = __import__(mo.group(1))
+            for name, obj in inspect.getmembers(module):
+                if inspect.isclass(obj):
+                    if issubclass(obj, model.Manager):
+                        if hasattr(obj, "name") and obj.name and obj.name not in seen_managers:
+                            try:
+                                srv.register_manager(obj)
+                                seen_managers.append(obj.name)
+                            except:
+                                print "Failed to register manager ", obj, " in module", mo.group(1)
+                                raise
+            srv.register_functions_from_module(module)
+                     
+        
+        srv.register_manager(authentication.NullAuthenticationManager)
+        srv.enable_global_functions()
+        srv.enable_documentation()
+        srv.enable_static_documents(os.path.join(adhoc_home, 'docroot'))
+        srv.enable_digs_and_updates()
+        srv.serve_forever()
 
-for file in os.listdir(serverdir):
-    mo = re.match(r"^([a-z_]+).py$", file)
-    if not mo:
-        continue
-    module = __import__(mo.group(1))
-    for name, obj in inspect.getmembers(module):
-        if inspect.isclass(obj):
-            if issubclass(obj, model.Manager):
-                if hasattr(obj, "name") and obj.name and obj.name not in seen_managers:
-                    try:
-                        srv.register_manager(obj)
-                        seen_managers.append(obj.name)
-                    except:
-                        print "Failed to register manager ", obj, " in module", mo.group(1)
-                        raise
-    srv.register_functions_from_module(module)
-             
 
-srv.register_manager(authentication.NullAuthenticationManager)
-srv.enable_global_functions()
-srv.enable_documentation()
-srv.enable_static_documents(os.path.join(adhoc_home, 'docroot'))
-srv.enable_digs_and_updates()
-srv.serve_forever()
+if __name__ == "__main__":
+    import sys, os
+    
+    if len(sys.argv) > 1:
+        if ':' in sys.argv[1]:
+            host, port = sys.argv[1].split(':')
+            port = int(port)
+        else:
+            host = 'localhost'
+            port = int(sys.argv[1])
+    else:
+        host, port = 'localhost', 4433
+
+    generic_password = os.environ.get("ADHOC_GENERIC_PASSWORD", None)
+
+    enable_ssl = os.environ.get("ADHOC_SSL_ENABLE", False)
+    
+    if enable_ssl:
+        print "Serving HTTPS on '%s' port %d." % (host, port)
+    else:
+        print "Serving HTTP on '%s' port %d." % (host, port)
+        
+    starter = StartMe(host, port, generic_password=generic_password, enable_ssl=enable_ssl)
+    starter.serve_forever()
+    
