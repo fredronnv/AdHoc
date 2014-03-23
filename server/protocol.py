1bcdc58a63579bd92438502aad9415174ab99978
Christer Bernérus <bernerus@CHALMERS.SE>
2014-03-23 20:47:01 +0000

    Changed dhcpd protocol so that the server does not have to select DHCP server. That is accomplished automatically. Added /auto mode which will simply reyurn a "niothing new" message if there are no chaged since the last fetch, which is stored in the dhcp_servers table. New url is typically https://adhoc.ita.chalmers.se/dhcpd/auto using auto mode. Omit /auto and the whole configuration will be delivered anyway.
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@338 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

--- server/protocol.py
+++ server/protocol.py
@@ -1,22 +1,66 @@
 from rpcc import *
+import socket
+
+
 
 
 class DhcpdConfProtocol(Protocol):
+    def fake_session(self, api_version, remote_ip, httphandler):
+        api = self.server.api_handler.get_api(api_version)
+        funobj = api.get_function_object("dhcp_server_fetch", httphandler)
+        funobj.call_time=datetime.datetime.now()
+        
+        if not self.server.database:
+            raise ExtInternalError("Function %s uses database, but no database is defined" % (funobj,))
+        funobj.set_db_link(self.server.database.get_link())
+        
+        session_manager = funobj.session_manager
+        session = session_manager.create_session(remote_ip)
+        return session
+            
     def request(self, httphandler, path, data):
-        # [/dhcpd/]servername
-        print "PATH=", path, " DATA=", data
+        # [/dhcpd/api][/auto]
+        #print "PATH=", path, " DATA=", data
         pathcomp = path.split("/")
         
-        if len(pathcomp) != 2:
-            return HTTPResponse("400 Bad request.", ctype="text/plain", code=400)
+        if len(pathcomp) < 1:
+            return HTTPResponse("400 Bad request. path=%s"%path, ctype="text/plain", code=400)
 
-        
         api_version = int(pathcomp[0])
-        dhcp_server_name = pathcomp[1]
-        
+        auto=False
+        if len(pathcomp) > 1 and pathcomp[1] == 'auto':
+            auto=True
         try:
-            res = self.server.call_rpc(httphandler, "dhcpd_config", [dhcp_server_name], api_version)
+            
+            remote_ip = httphandler.client_address[0]
+            (remote_dns, revip, ip)  = socket.gethostbyaddr(remote_ip)
+            #print "Remote DNS=",remote_dns
+            session = self.fake_session(api_version, remote_ip, httphandler)
+            try:
+                res = self.server.call_rpc(httphandler, "dhcp_server_dig", [session, {'dns': remote_dns}, {'dhcp_server':True, "dns":True, "latest_fetch":True}], api_version)
+            except Exception, e:
+                print "dhcp_server_fetch error", e
+                raise
+            if len(res["result"])== 0:
+                   return HTTPResponse("403 Forbidden. Data is only accessible for registered hosts", code=403)
+            #return HTTPResponse("543 Debug stop. res=%s"%res, ctype="text/plain", code=543)
+            dhcp_server_id = res["result"][0]["dhcp_server"]
+            latest_fetch = res["result"][0]["latest_fetch"]
+            maxid = self.server.call_rpc(httphandler, "event_get_max_app_id",[session], api_version)["result"]
+        
+            if auto and maxid <= latest_fetch:
+                return HTTPResponse(("Nothing new since last fetch at event id %d."%(latest_fetch)).encode("utf-8"), ctype="text/html", encoding="utf-8")
+                
+            db = self.server.database.get_link()
+            q = "UPDATE dhcp_servers SET latest_fetch=:maxid WHERE id=:id"
+            db.put(q, maxid=maxid, id=dhcp_server_id)
+            db.commit()
+            
+            res = self.server.call_rpc(httphandler, "dhcpd_config", [dhcp_server_id], api_version)
             s=res["result"]
             return HTTPResponse(s.encode("utf-8"), ctype="text/html", encoding="utf-8")
-        except:
+        
+        except Exception, e:
+            print e
+            raise
             return HTTPResponse("500 Internal server error.", ctype="text/plain", code=500)
