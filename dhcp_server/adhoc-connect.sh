c85c1c708021242f2ee29a448050d9b2bcf99baf
Christer Bernérus <bernerus@CHALMERS.SE>
2014-04-24 07:49:33 +0000

    Initial checkin of fetcher script.
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@351 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

---
+++ dhcp_server/adhoc-connect.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+TMP_CONF=/etc/dhcp/dhcpd.conf.new
+DHCPD_CONF=/etc/dhcp/dhcpd.conf
+ADHOC_URL=https://adhoc.ita.chalmers.se:8877
+ADHOC_API_VERSION=0
+LOGFILE=/var/log/adhoc-connect.log
+AUTO=auto
+
+log()
+{
+        msg="`date '+%F %T'` INFO  $@"
+        echo "$msg" >>$LOGFILE
+}
+
+err()
+{
+        msg="`date '+%F %T'` ERROR $@"
+        echo "$msg" >>$LOGFILE
+}
+
+fatal()
+{
+        msg="`date '+%F %T'` FATAL $@"
+        echo "$msg" >>$LOGFILE
+        exit 1
+}
+
+install_conf()
+{
+	# Do syntax check first
+    if [ /usr/sbin/dhcpd -t -cf ${TMP_CONF} >/dev/null 2>&1 ]; then
+        fatal "$@ not approved by dhcpd"
+    else
+    	# Save old config in case restarting server fails
+        mv ${DHCPD_CONF} ${DHCPD_CONF}.bak || fatal "Failed to move away ${DHCPD_CONF} to backup file"
+        mv ${TMP_CONF} ${DHCPD_CONF} || fatal "Failing to install fetched dhcpd.conf"
+    fi
+    log "$@ conf installed"
+    if /etc/init.d/dhcpd restart >/dev/null 2>&1 ;  then
+        :
+    else
+        err "Failed to restart dhcpd" # This can happen for semantic reasons not caught in syntax check
+        mv ${DHCPD_CONF}.bak ${DHCPD_CONF} || fatal "Failed to restore ${DHCPD_CONF} from backup file"
+        log "Previous dhcpd.conf file restored"
+        /etc/init.d/dhcpd restart >/dev/null 2>&1
+        log "dhcpd restarted with previous configuration"
+        return
+    fi
+    log "dhcpd restarted with new configuration"
+}
+
+touch ${DHCPD_CONF}  # Makes sure it exists
+if [ `egrep -v '^#' ${DHCPD_CONF} | wc -l` -lt 10 ]; then
+        wget -q -O ${TMP_CONF} ${ADHOC_URL}/dhcpd/${ADHOC_API_VERSION} || fatal "Failed fetching dhcpd.conf from ${ADHOC_URL}"
+        install_conf "Initial dhcpd.conf"
+        exit 0
+fi
+
+wget -q -O  ${TMP_CONF} ${ADHOC_URL}/dhcpd/${ADHOC_API_VERSION}/${AUTO} || fatal "Failed fetching ${AUTO} dhcpd.conf from ${ADHOC_URL}"
+if [ `cat ${TMP_CONF} | wc -l` -gt 10 ]; then
+        install_conf "Updated dhcpd.conf"
+        exit 0
+fi
