20acb04732b0903db99ee19a9a05f12bbeac08de
Christer Bernérus <bernerus@CHALMERS.SE>
2014-05-07 10:08:27 +0000

    Do not change deploy lebel if already set.
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@430 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

--- server/bin/startserver.sh
+++ server/bin/startserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export CHALMERS_DEPLOY_LEVEL="prod"
+export CHALMERS_DEPLOY_LEVEL=${CHALMERS_DEPLOY_LEVEL:prod}
 export INSTALL_HOME=`(cd ~srvadhoc/adhoc-server; pwd)`
 . $INSTALL_HOME/etc/bashrc.public
 . $INSTALL_HOME/etc/bashrc.private
-exec python -u $ADHOC_RUNTIME_HOME/bin/adhocserv.py `hostname`:8877
+exec python -u $ADHOC_RUNTIME_HOME/bin/adhocserv.py $ADHOC_SERVER_HOST:ADHOC_SERVER_PORT
