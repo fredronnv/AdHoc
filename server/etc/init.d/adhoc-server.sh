9465ae56d467afec3cdbe1e577d8f18ae9980f5d
Christer Bernérus <bernerus@CHALMERS.SE>
2014-08-06 08:24:44 +0000

    Renamed the server startup script to conform with other naming. Updated setup instructions. Changed home och srvadhoc. Reordered steps, added more info. Added upgrade instructions. Added reference to setup instructions to the README file.
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@501 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

--- server/etc/init.d/adhoc-server.sh
+++ server/etc/init.d/adhoc-server.sh
@@ -10,12 +10,15 @@
 export ADHOC_USER=${ADHOC_USER:-srvadhoc}
 ADHOC_USER_HOME=$(eval echo ~${ADHOC_USER})
 ADHOC_RUNTIME_HOME=${ADHOC_USER_HOME}/adhoc-server
-PIDDIR=${ADHOC_RUNTIME_HOME}/var/run
-LOGDIR=${ADHOC_RUNTIME_HOME}/var/log
+PIDDIR=${ADHOC_USER_HOME}/var/run
+LOGDIR=${ADHOC_USER_HOME}/var/log
 export CHALMERS_DEPLOY_LEVEL=prod
 
 start()
 {
+	mkdir -p ${PIDDIR}
+	mkdir -p ${LOGDIR}
+	chown ${ADHOC_USER} ${PIDDIR} ${LOGDIR}
     echo -n Starting AdHoc server:
 
 	/usr/local/bin/daemon -P ${PIDDIR} -i --env="HOME=$ADHOC_USER_HOME" --name=adhoc -r \
