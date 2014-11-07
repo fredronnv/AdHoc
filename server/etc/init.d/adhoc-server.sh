7c8f7a18bc6fe6d330d64d54133fb48884b3be39
Christer Bernérus <bernerus@CHALMERS.SE>
2014-11-07 09:14:23 +0000

    Moved the etc directory back inte the server so that it will be distributed. The task of setting up etc at the server distribution's parent level is manual.
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@555 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

---
+++ server/etc/init.d/adhoc-server.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+# chkconfig: 35 90 12
+# description: AdHoc server
+#
+# Get function from functions library
+. /etc/init.d/functions
+# Start the AdHoc server
+
+export ADHOC_USER=${ADHOC_USER:-srvadhoc}
+ADHOC_USER_HOME=$(eval echo ~${ADHOC_USER})
+ADHOC_RUNTIME_HOME=${ADHOC_USER_HOME}/adhoc-server
+VARDIR=${ADHOC_USER_HOME}/var
+PIDDIR=${VARDIR}/run
+LOGDIR=${VARDIR}/log
+export CHALMERS_DEPLOY_LEVEL=${CHALMERS_DEPLOY_LEVEL:prod}
+
+start()
+{
+	mkdir -p ${PIDDIR}
+	mkdir -p ${LOGDIR}
+	chown ${ADHOC_USER} ${VARDIR} ${PIDDIR} ${LOGDIR}
+    echo -n Starting AdHoc server:
+
+	/usr/local/bin/daemon -P ${PIDDIR} -i --env="HOME=$ADHOC_USER_HOME" --name=adhoc -r \
+		--user=${ADHOC_USER} --delay=1800 -l ${LOGDIR}/daemon.log \
+        --output=${LOGDIR}/AdHoc.log -- ${ADHOC_RUNTIME_HOME}/bin/startserver.sh
+    success "AdHoc server startup"
+    echo
+}
+# Stop the service AdHoc
+stop()
+{
+    echo -n Stopping AdHoc server:
+    /usr/local/bin/daemon -P ${PIDDIR} --name=adhoc --stop
+    echo
+}
+
+restart()
+{
+        echo -n "Restarting AdHoc server: "
+        /usr/local/bin/daemon -P ${PIDDIR} --name=adhoc --restart
+}
+
+status()
+{
+        if /usr/local/bin/daemon -P ${PIDDIR} --name=adhoc --running; then
+                echo The AdHoc server is running
+        else
+                echo The AdHoc server is not running
+        fi
+}
+
+### main logic ###
+case "$1" in
+  start)
+        start
+        ;;
+  stop)
+        stop
+        ;;
+  status)
+        status AdHoc
+        ;;
+  restart|reload|condrestart)
+        restart
+        ;;
+  *)
+        echo "Usage: $0 {start|stop|restart|status}"
+        exit 1
+esac
+exit 0
