60c79e7063a452798595498c373e6f213335b298
Christer Bernérus <bernerus@CHALMERS.SE>
2014-12-10 13:12:26 +0000

    Added a scripts library where AdHoch can be run interactively from python.
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@653 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

---
+++ applications/scripts/main.py
@@ -0,0 +1,29 @@
+import rpcc_client
+import getpass
+def connect(url="https://adhoc.ita.chalmers.se:8877", user="bernerus"):
+    try:
+	return do_connect(url, user)
+    except Exception, e:
+	print e
+	try:
+	    return do_connect(url="https://test-cdks.ita.chalmers.se:4434",  user=user)
+	except:
+	    return do_connect(url="https://cdks.chalmers.se:4434", user=user)
+
+def do_connect(url, user):
+    global p
+    print "Connecting to",url
+    p=rpcc_client.RPCC(url)
+    #print "Enter password for "+user
+    pw=getpass.getpass()
+    if not p.session_auth_login(user, pw):
+            raise ValueError
+    print "Connected to",url
+    return p
+    #print p.server_list_functions()
+
+def connect_krb5(url="https://adhoc.ita.chalmers.se:8877"):
+    global p
+    p=rpcc_client.RPCC(url)
+    p.session_auth_kerberos()
+    return p
