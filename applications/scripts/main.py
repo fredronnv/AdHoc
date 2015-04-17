6a8767675e9521cdf06c25680e1290cc6a9e31ac
Christer Bernérus <bernerus@CHALMERS.SE>
2015-04-17 11:26:25 +0000

    Changed to use krb5 authentication.
    git-svn-id: https://svn.ita.chalmers.se/repos/adhoc/trunk@718 9cf5bdc2-3f39-413b-b911-f617d3cba2d0

--- applications/scripts/main.py
+++ applications/scripts/main.py
@@ -1,29 +1,33 @@
 import rpcc_client
 import getpass
+
+
 def connect(url="https://adhoc.ita.chalmers.se:8877", user="bernerus"):
     try:
-	return do_connect(url, user)
+        return connect_krb5(url)
     except Exception, e:
-	print e
-	try:
-	    return do_connect(url="https://test-cdks.ita.chalmers.se:4434",  user=user)
-	except:
-	    return do_connect(url="https://cdks.chalmers.se:4434", user=user)
+        print e
+        try:
+            return do_connect(url="https://test-cdks.ita.chalmers.se:4434", user=user)
+        except:
+            return do_connect(url="https://cdks.chalmers.se:4434", user=user)
+
 
 def do_connect(url, user):
     global p
-    print "Connecting to",url
-    p=rpcc_client.RPCC(url)
-    #print "Enter password for "+user
-    pw=getpass.getpass()
+    print "Connecting to", url
+    p = rpcc_client.RPCC(url)
+    # print "Enter password for "+user
+    pw = getpass.getpass()
     if not p.session_auth_login(user, pw):
             raise ValueError
-    print "Connected to",url
+    print "Connected to", url
     return p
-    #print p.server_list_functions()
+    # print p.server_list_functions()
+
 
 def connect_krb5(url="https://adhoc.ita.chalmers.se:8877"):
     global p
-    p=rpcc_client.RPCC(url)
+    p = rpcc_client.RPCC(url)
     p.session_auth_kerberos()
     return p
